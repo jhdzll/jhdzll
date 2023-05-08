ee24fc75ed6b5f628c81d2d327e1538b669aec95
jhd <jhdzll521@163.com>
2023-05-08 18:17:26 +0800

    second

---
+++ src/api/login.js
@@ -0,0 +1,8 @@
+import request from "@/utils/request";
+export function login(query) {
+    return request({
+        url: 'login',
+        method: 'get',
+        params: query
+    })
+}
---
+++ src/views/LoginView.vue
@@ -0,0 +1,38 @@
+<template>
+  <a-form layout="inline" :model="formState" @finish="handleFinish" @finishFailed="handleFinishFailed">
+    <a-form-item>
+      <a-input v-model:value="formState.user" placeholder="Username">
+        <template #prefix>
+          <UserOutlined style="color: rgba(0, 0, 0, 0.25)" />
+        </template>
+      </a-input>
+    </a-form-item>
+    <a-form-item>
+      <a-input v-model:value="formState.password" type="password" placeholder="Password">
+        <template #prefix>
+          <LockOutlined style="color: rgba(0, 0, 0, 0.25)" />
+        </template>
+      </a-input>
+    </a-form-item>
+    <a-form-item>
+      <a-button type="primary" html-type="submit" :disabled="formState.user === '' || formState.password === ''">
+         登 录
+      </a-button>
+    </a-form-item>
+  </a-form>
+</template>
+<script setup lang="ts">
+import { reactive } from 'vue';
+
+const formState = reactive({
+  user: '222',
+  password: '11111',
+});
+
+const handleFinish = (values: any) => {
+  console.log(formState.user);
+};
+const handleFinishFailed = (errors:any) => {
+  console.log(errors);
+};
+</script>
